b4b176602343efa52d55abef55e8ea1075b3bd2e
Karan Jaswal <118061359+Jaswal-12@users.noreply.github.com>
2026-01-25 22:17:02 +0530

    Implement palindrome check in Valid_Palindrome.py

--- Strings/Valid_Palindrome.py
+++ Strings/Valid_Palindrome.py
@@ -1,1 +1,30 @@
+#Leetcode-125
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        s=s.toLowerCase();
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(Character.isLetterOrDigit(s.charAt(i))){
+                sb.append(s.charAt(i));
+            }
+        }
 
+
+        String x=sb.toString();
+        int left=0;
+        int right=x.length()-1;
+
+        while(left<right){
+            if(x.charAt(left)!=x.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+
+
+    }
+}
